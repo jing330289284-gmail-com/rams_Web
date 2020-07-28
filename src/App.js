eb0e4e6e443df4b620036ccf55a8a92fe4afad73
jingda19940418 <jing330289284@gmail.com>
2020-07-28 17:08:58 +0900

    path更新

--- src/App.js
+++ src/App.js
@@ -13,33 +13,35 @@ import CustomerInfo from './components/CustomerInfo';
 import TopCustomerInfo from './components/topCustomerInfo';
 import TechnologyTypeMaster from './components/technologyTypeMaster';
 import CustomerInfoSearch from './components/customerInfoSearch';
+import siteInformation from './components/SiteInformation';
 
 function App() {
-  const marginTop = {
-    marginTop: "20px"
-  }
-  return (
-    <Router>
-      <div style={{"backgroundColor":"#f5f5f5"}}>
+	const marginTop = {
+		marginTop: "20px"
+	}
+	return (
+		<Router>
+			<div style={{ "backgroundColor": "#f5f5f5" }}>
 
-        <Route exact path="/" component={Login}/>
-        <Route path="/subMenu" component={SubMenu}/>
-        <Route path="/subCost" component={Subcost}/>
-        <Route path="/main" component={main} />
-        <Route path="/add" component={mainAdd} />
-        <Route path="/edit/:employeeNo" component={messegeAlert} />
-        <Route path="/mainSearch" component={mainSearch} />
-        <Route path="/mainSearchTest" component={mainSearchTest} />
-        <Route path="/messegeAlert" component={messegeAlert} />
-        <Route path="/bankInfo" component={BankInfo} />
-        <Route path="/customerInfo" component={CustomerInfo} />
-        <Route path="/topCustomerInfo" component={TopCustomerInfo} />
-        <Route path="/technologyTypeMaster" component={TechnologyTypeMaster} />
-        <Route path="/customerInfoSearch" component={CustomerInfoSearch} />
-      </div>
-    </Router>
+				<Route exact path="/" component={Login} />
+				<Route path="/subMenu" component={SubMenu} />
+				<Route path="/subCost" component={Subcost} />
+				<Route path="/main" component={main} />
+				<Route path="/add" component={mainAdd} />
+				<Route path="/edit/:employeeNo" component={messegeAlert} />
+				<Route path="/mainSearch" component={mainSearch} />
+				<Route path="/mainSearchTest" component={mainSearchTest} />
+				<Route path="/messegeAlert" component={messegeAlert} />
+				<Route path="/bankInfo" component={BankInfo} />
+				<Route path="/customerInfo" component={CustomerInfo} />
+				<Route path="/topCustomerInfo" component={TopCustomerInfo} />
+				<Route path="/technologyTypeMaster" component={TechnologyTypeMaster} />
+				<Route path="/customerInfoSearch" component={CustomerInfoSearch} />
+				<Route path="/siteInformation" component={siteInformation} />
+			</div>
+		</Router>
 
-  );
+	);
 }
 
 export default App;
