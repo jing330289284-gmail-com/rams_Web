60e6cb552f09d25ca8715e2c419b7eba22f4183d
1831745821 <1831745821@qq.com>
2020-07-29 23:09:56 +0900

    位置のURLを修正

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../asserts/css/style.css';
+import './asserts/css/index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
