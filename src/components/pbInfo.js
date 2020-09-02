4e363e48fed0f4aa15cef9af7cd56ba35a32f75d
2709505264 <65960727+2709505264@users.noreply.github.com>
2020-09-02 14:01:07 +0900

    Add files via upload
    shi

--- src/components/pbInfo.js
+++ src/components/pbInfo.js
@@ -7,16 +7,16 @@ import DatePicker, { registerLocale } from "react-datepicker"
 import { faSave, faUndo, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as utils from './utils/publicUtils.js';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 class pbInfo extends React.Component {
     state = { 
-	    customerName:[],//BP所属
-        unitPrice: '',//単価
-        reflectStartDate: '',//営業状況
-        salesProgress:[],//営業状況
-		salesProgressOnSelect: '',//選択中の営業状況
+		bpSalesProgress:[],//営業状況
+		bpBelongCustomerCode:'',//選択中のBP所属
+        bpUnitPrice: '',//単価
+		bpSalesProgressCode: '',//選択中の営業状況
         bpOtherCompanyAdmissionEndDate:'',//所属現場終年月
-        remark:'',//備考
+        bpRemark:'',//備考
 		pbInfo:'',//入力データ
 		actionType:'',//処理区分
      }
@@ -40,31 +40,47 @@ class pbInfo extends React.Component {
 	    var actionType = this.props.actionType;//父画面のパラメータ（処理区分）
 		var pbInfo = this.props.pbInfo;//父画面のパラメータ（画面既存諸費用情報）
 //営業状況リスト取得
-	    var salesProgress = utils.getdropDown("getSalesProgress");
-        for(let i = 1;i<salesProgress.length ; i++){
-            $("#salesProgress").append('<option value="'+salesProgress[i].code+'">'+salesProgress[i].name+'</option>');
+	    var bpSalesProgress = utils.getdropDown("getSalesProgress");
+        for(let i = 1;i<bpSalesProgress.length ; i++){
+            $("#bpSalesProgressCode").append('<option value="'+bpSalesProgress[i].code+'">'+bpSalesProgress[i].name+'</option>');
         }
- 		$("#salesProgress").find("option[value='4']").attr("selected",true);
-            pbInfo = this.props.pbInfo;//父画面のパラメータ（画面既存の新パスワード）
+ 		$("#bpSalesProgressCode").find("option[value='4']").attr("selected",true);
             if(this.props.employeeFristName===undefined||this.props.employeeLastName===undefined){
                 $('#pbInfoEmployeeName').val(" "); 
              }else{
                  document.getElementById("pbInfoEmployeeName").innerHTML =  this.props.employeeFristName + this.props.employeeLastName;
                 }
-    }
+    if(!$.isEmptyObject(pbInfo)){
+		$("#bpBelongCustomerCode").val(pbInfo.bpBelongCustomerCode);
+        $("#bpUnitPrice").val(pbInfo.bpUnitPrice);
+		$("#bpSalesProgressCode").val(pbInfo.bpSalesProgressCode);
+		 this.setState({
+            bpOtherCompanyAdmissionEndDate:utils.converToLocalTime(pbInfo.bpOtherCompanyAdmissionEndDate,false),
+        })
+
+		$("#bpRemark").val(pbInfo.bpRemark);
+		}    
+	}
     /**
  	* 登録ボタン
      */
     pbInfoTokuro=()=>{
-          	pbInfo["customerName"] = $("#customerName").val();
-            pbInfo["unitPrice"] = $("#unitPrice").val();
-            pbInfo["salesProgressName"] = $("#salesProgressName").val();
+			var pbInfo = {};
+          	pbInfo["bpBelongCustomerCode"] = $("#bpBelongCustomerCode").val();
+            pbInfo["bpUnitPrice"] = $("#bpUnitPrice").val();
+            pbInfo["bpSalesProgressCode"] = $("#bpSalesProgressCode").val();
             pbInfo["bpOtherCompanyAdmissionEndDate"] = utils.formateDate(this.state.bpOtherCompanyAdmissionEndDate,false);
             pbInfo["bpRemark"] = $("#bpRemark").val();
             this.props.pbInfoTokuro(pbInfo);
 	}
+	reset=()=>{
+	  $("#bpBelongCustomerCode").val("");
+	  $("#bpUnitPrice").val("");
+	  $("#bpSalesProgressCode").val("4");
+	  $("#bpRemark").val("");
+	}
     render() {
-	const { actionType} = this.state;
+	const { actionType,bpBelongCustomerCode} = this.state;
         return (
             <div>
                 <Row inline="true">
@@ -90,34 +106,46 @@ class pbInfo extends React.Component {
                 </Row>
                 <Row>
                     <Col sm={6}>
-                    	<InputGroup size="sm" className="mb-3">
-							<InputGroup.Prepend>
-								<InputGroup.Text id="inputGroup-sizing-sm">BP所属{'\u00A0'}{'\u00A0'}{'\u00A0'}</InputGroup.Text>
-						        <Form.Control type="pbInfo" id="customerName"a name="customerName" />
-                      		</InputGroup.Prepend>
-						</InputGroup>
+								<InputGroup size="sm" className="mb-3">
+									<InputGroup.Prepend>
+										<InputGroup.Text id="inputGroup-sizing-sm">BP所属</InputGroup.Text>
+										<Autocomplete
+											id="bpBelongCustomerCode"
+											name="bpBelongCustomerCode"
+											value={bpBelongCustomerCode}
+											options={this.props.customer}
+											getOptionLabel={(option) => option.name}
+											renderInput={(params) => (
+												<div ref={params.InputProps.ref}>
+													<input placeholder="BP所属" type="text" {...params.inputProps}
+														style={{ width: 145, height: 31, borderColor: "#ced4da", borderWidth: 1, borderStyle: "solid", fontSize: ".875rem", color: "#495057" }} />
+												</div>
+											)}
+										/>
+								</InputGroup.Prepend>
+							</InputGroup>
 	                </Col>
                     <Col sm={6}>
                         <InputGroup size="sm" className="mb-3">
 							<InputGroup.Prepend>
-								<InputGroup.Text id="inputGroup-sizing-sm">BP単価{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</InputGroup.Text>
-	                 	        <Form.Control type="pbInfo" id="unitPrice"a name="unitPrice" maxlength='5'/>
-								<InputGroup.Text id="inputGroup-sizing-sm">万円</InputGroup.Text>
+								<InputGroup.Text id="pbInfoSetText">BP単価{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</InputGroup.Text>
+	                 	        <Form.Control type="text" id="bpUnitPrice"a name="bpUnitPrice" maxlength='5'/>
+								<InputGroup.Text id="pbInfoSetText">万円</InputGroup.Text>
 							</InputGroup.Prepend>
                         </InputGroup>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col sm={6}>
                         <InputGroup size="sm" className="mb-3">
                             <InputGroup.Prepend>
                                 <InputGroup.Text id="pbInfoSetText">営業状況</InputGroup.Text>
-								<Form.Control id="salesProgress" name="salesProgress" as="select" >
+								<Form.Control id="bpSalesProgressCode" name="bpSalesProgressCode" as="select" >
                           		</Form.Control>
                             </InputGroup.Prepend> 
                         </InputGroup>
                     </Col>
-                    <Col>
+                    <Col sm={6}>
                         <InputGroup size="sm" className="mb-3">
                             <InputGroup.Prepend>
                                 <InputGroup.Text id="pbInfoSetText">所属現場終年月</InputGroup.Text>                       
@@ -146,12 +174,12 @@ class pbInfo extends React.Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col sm={6}>
                         <InputGroup size="sm" className="mb-3">
                             <InputGroup.Prepend>
                                 <InputGroup.Text id="inputGroup-sizing-sm">備考{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}</InputGroup.Text>
-                                <Form.Control type="pbInfo" id="pbInfoCheck"a name="pbInfoCheck" />
-                            </InputGroup.Prepend>
+                                <FormControl id="bpRemark" placeholder="例：XXXXX" name="bpRemark" type="text" aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                        	</InputGroup.Prepend>
                         </InputGroup>                       
                     </Col>
                 </Row>
@@ -163,7 +191,7 @@ class pbInfo extends React.Component {
                                 </Button>
                         </Col>
                         <Col sm={3} className="text-center">
-                                <Button  block size="sm" id="reset" type="reset" variant="info">
+                                <Button  block size="sm" id="reset" type="reset" variant="info" value="Reset" onclick="reset">
                                 <FontAwesomeIcon icon={faEdit} />リセット
                                 </Button>
                         </Col>
