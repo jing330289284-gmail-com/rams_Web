2c1c7580ed498797c65e97540c6101aee11d51e9
劉林涛 <@DESKTOP-SPU4BNM>
2020-11-04 16:35:02 +0900

    劉林涛

--- src/components/services/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import dropDownReducer from './dropDownReducer';
-
-const rootReducer = combineReducers({
-    data: dropDownReducer,
-});
-
-export default rootReducer;
