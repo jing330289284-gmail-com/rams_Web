6b3c2fe483b1586d1e58dba34df61226ceb22b6c
wang <wangy@PJNTPB-JA01>
2021-04-05 16:44:11 +0900

    ec2

--- src/components/redux/reducer.js
+++ src/components/redux/reducer.js
@@ -72,8 +72,8 @@ export function fetchDropDown() {
 		"getServerIP",// 最後
 	]
 	var outArray = [];
-	var serverIP = "http://127.0.0.1:8080/";
-	// var serverIP = "http://34.219.63.92:8080/";
+	// var serverIP = "http://127.0.0.1:8080/";
+	var serverIP = "http://34.219.63.92:8080/";
 
 	var par = JSON.stringify(methodNameList);
 	$.ajax({
@@ -174,8 +174,8 @@ var methodNameList = ["getGender",// 0.性別区別
 		"getServerIP",// 最後
 	]
 var outArray = [];
-var serverIP = "http://127.0.0.1:8080/";
-//var serverIP = "http://34.219.63.92:8080/";
+// var serverIP = "http://127.0.0.1:8080/";
+var serverIP = "http://34.219.63.92:8080/";
 
 var par = JSON.stringify(methodList);
 $.ajax({
